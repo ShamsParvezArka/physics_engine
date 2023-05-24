e4eacc0bfe818c5f37a48c0c6d1420f6ee450328
Shams Parvez Arka <parvez6826@gmail.com>
2023-05-24 21:02:10 +0600

    MORE POWAAH!

--- main.c
+++ main.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <raylib.h>
 
-typedef struct Node {
-	float x;
-	float y;
-	struct Node *next;
-} node;
+#define LIST_IMPLEMENTATION
+#include "list.h"
 
-#define NODE_MALLOC ((node*) malloc(sizeof(node)));
-
-static node *root, *spoint;
-
-void push(float x, float y) {
-	node *new_node = NODE_MALLOC;
-	new_node->x = x;
-	new_node->y = y;
-	new_node->next = root;
-	root = new_node;
-}
+#define PHYSICS_IMPELMENTATION
+#include "physics.h"
 
 int main() 
 {
-	const int window_width  = 1200;
-	const int window_height =  800;
+	// INITIALIZE
+	const int window_width  = 600;
+	const int window_height = 600;
 
-	InitWindow(window_width, window_height, "gravity");
+	InitWindow(window_width, window_width, "gravity");
 	SetTargetFPS(60);
 
-	spoint = NODE_MALLOC;
-	while(!WindowShouldClose()) {
-		ClearBackground(RAYWHITE);
+	Rectangle rec = {window_width/2, window_height/2, 30, 30};
+	node *spoint = NODE_MALLOC;
 
-		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-			push(GetMouseX(), GetMouseY());
+	while(!WindowShouldClose()) {
+		// UPDATE
+		ClearBackground(RAYWHITE);
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+			Rectangle rect = {GetMouseX(), GetMouseY(), 30, 30};
+			push_rect(rect);
 		}
 
+		spoint = root;
+		while (spoint != NULL) {
+			update_gravity(&spoint->obj, window_width, window_height);
+			spoint = spoint->next;
+		}
+
+		// RENDER
 		BeginDrawing();
 		{
 			spoint = root;
-			while(spoint != NULL) {
-				DrawRectangle(spoint->x, spoint->y, 20, 20, BLACK);
+			while (spoint != NULL) {
+				DrawRectangleRec(spoint->obj, BLACK);
 				spoint = spoint->next;
 			}
 		}
